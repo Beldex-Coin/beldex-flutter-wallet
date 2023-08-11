6c52c61cdaaa7ae8fb1074ecc7007aa53fa2367f
SteveJosephh21 <steve.josephh@beldex.io>
2023-08-11 19:01:07 +0530

    Fix : Not navigated to next screen while clicking the content in restore from seed/keys options screen.

--- lib/src/screens/restore/restore_options_page.dart
+++ lib/src/screens/restore/restore_options_page.dart
@@ -1,161 +1,74 @@
 import 'package:beldex_wallet/src/stores/settings/settings_store.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:beldex_wallet/palette.dart';
 import 'package:beldex_wallet/routes.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:beldex_wallet/src/screens/restore/widgets/restore_button.dart';
-import 'package:beldex_wallet/src/screens/restore/widgets/image_widget.dart';
-import 'package:beldex_wallet/src/screens/restore/widgets/base_restore_widget.dart';
 import 'package:beldex_wallet/src/screens/base_page.dart';
 import 'package:beldex_wallet/generated/l10n.dart';
 import 'package:provider/provider.dart';
 
 class RestoreOptionsPage extends BasePage {
-  static const _aspectRatioImage = 2.086;
 
   @override
   String get title => S.current.restore_restore_wallet;
 
-  // @override
-  // Widget leading(BuildContext context) {
-  //   return Container(
-  //       padding: const EdgeInsets.only(top: 12.0, left: 10),
-  //       decoration: BoxDecoration(
-  //         //borderRadius: BorderRadius.circular(10),
-  //         //color: Colors.black,
-  //       ),
-  //       child: SvgPicture.asset('assets/images/beldex_logo_foreground1.svg'));
-  // }
-
   @override
   Color get backgroundColor => Palette.creamyGrey;
 
-  //final _imageSeedKeys = Image.asset('assets/images/seedKeys.png');
-  //final _imageRestoreSeed = Image.asset('assets/images/restoreSeed.png');
+  @override
+  Widget trailing(BuildContext context){
+    return Container();
+  }
 
   @override
   Widget body(BuildContext context) {
-    final isLargeScreen = MediaQuery.of(context).size.height > largeHeight;
     final settingsStore = Provider.of<SettingsStore>(context);
     return Column(
       children: [
         Card(
           margin: EdgeInsets.only(left: 20,right: 20,top: 50),
-          elevation:0, //5,
+          elevation:0,
           color:settingsStore.isDarkTheme ? Color(0xff272733) : Color(0xffEDEDED), //Theme.of(context).cardColor,//Color.fromARGB(255, 40, 42, 51),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10)
           ),
-          child:Padding(
-            padding: const EdgeInsets.all(30.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(S.of(context).restore_title_from_seed_keys,style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),),
-                      SizedBox(height: 15),
-                      Text(S.of(context).restore_description_from_seed_keys,style: TextStyle(fontSize: 12,fontWeight: FontWeight.normal,
-                      color: settingsStore.isDarkTheme ?  Color(0xffACACAC) : Color(0xff545454),
-                      ),
-                      ),
-                    ],
+          child:InkWell(
+            onTap: (){
+              Navigator.pushNamed(
+                  context, Routes.restoreWalletOptionsFromWelcome);
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(S.of(context).restore_title_from_seed_keys,style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),),
+                        SizedBox(height: 15),
+                        Text(S.of(context).restore_description_from_seed_keys,style: TextStyle(fontSize: 12,fontWeight: FontWeight.normal,
+                        color: settingsStore.isDarkTheme ?  Color(0xffACACAC) : Color(0xff545454),
+                        ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                InkWell(
-                  onTap: (){
-                    Navigator.pushNamed(
-                        context, Routes.restoreWalletOptionsFromWelcome);
-                  },
-                  child: Container(
-                    //elevation:0, //5,
-                   // margin: EdgeInsets.only(left:20),
+                  Container(
                     padding: EdgeInsets.all(9),
                     decoration: BoxDecoration(
                         shape: BoxShape.circle,
                          color: Color(0xff1BB71E),
-                    ), //Colors.white,
-                    
-                    child: Icon(Icons.arrow_forward_ios_rounded,color: Colors.white,size: 25,),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
-        /*Card(
-          margin: EdgeInsets.only(left: 40,right: 40,top: 20),
-          elevation: 5,
-          color: Color.fromARGB(255, 40, 42, 51),
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10)
-          ),
-          child:Padding(
-            padding: const EdgeInsets.all(30.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(S.of(context).restore_title_from_seed_keys,style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),),
-                      SizedBox(height: 10,),
-                      Text(S.of(context).restore_description_from_seed_keys,style: TextStyle(fontSize: 12,fontWeight: FontWeight.normal),)
-                    ],
-                  ),
-                ),
-                InkWell(
-                  onTap: (){
-
-                  },
-                  child: Card(
-                    elevation: 5,
-                    color: Colors.white,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8)
                     ),
-                    child: Icon(Icons.arrow_forward_ios_rounded,color: Theme.of(context).accentTextTheme.caption.decorationColor,size: 35,),
-                  ),
-                )
-              ],
+                    child: Icon(Icons.arrow_forward_ios_rounded,color: Colors.white,size: 25,),
+                  )
+                ],
+              ),
             ),
           ),
-        ),*/
+        ),
       ],
-    )/*BaseRestoreWidget(
-      firstRestoreButton: RestoreButton(
-        onPressed: () =>
-          Navigator.pushNamed(
-              context, Routes.restoreWalletOptionsFromWelcome),
-        imageWidget: ImageWidget(
-          image: _imageSeedKeys,
-          aspectRatioImage: _aspectRatioImage,
-          isLargeScreen: isLargeScreen,
-        ),
-        titleColor: Palette.lightViolet,
-        color: Palette.lightViolet,
-        title: S.of(context).restore_title_from_seed_keys,
-        description: S.of(context).restore_description_from_seed_keys,
-        textButton: S.of(context).restore_next,
-      ),
-      secondRestoreButton: RestoreButton(
-        onPressed: () {},
-        imageWidget: ImageWidget(
-          image: _imageRestoreSeed,
-          aspectRatioImage: _aspectRatioImage,
-          isLargeScreen: isLargeScreen,
-        ),
-        titleColor: BeldexPalette.teal,
-        color: BeldexPalette.teal,
-        title: S.of(context).restore_title_from_backup,
-        description: S.of(context).restore_description_from_backup,
-        textButton: S.of(context).restore_next,
-      ),
-      isLargeScreen: isLargeScreen,
-    )*/;
+    );
   }
 }
